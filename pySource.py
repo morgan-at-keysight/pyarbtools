273e649342f5a660c07062080a7592f99aecd53f
morgan-at-keysight <morgan.allison@keysight.com>
2018-10-05 18:06:44 -0400

    Moved separate example scripts to examples.py. Made some adjustments to resolution configuration in M8190A.configure().

--- pySource.py
+++ pySource.py
@@ -73,8 +73,7 @@ class M8190A(SocketInstrument):
 
     def configure(self, res='wsp', clkSrc='int', fs=7.2e9, refSrc='axi', refFreq=100e6, out1='dac', out2='dac', func1='arb', func2='arb', cf1=2e9, cf2=2e9):
         """Sets basic configuration for M8190A and populates class attributes accordingly."""
-        self.write(f'trace1:dwidth {res}')
-        self.res = self.query('trace1:dwidth?').strip().lower()
+        self.set_resolution(res)
 
         self.write(f'frequency:raster:source {clkSrc}')
         self.clkSrc = self.query('frequency:raster:source?').strip().lower()
