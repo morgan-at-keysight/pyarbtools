4082880252b80001cb07f210e82e93473a4c533f
morgan-at-keysight <morgan.allison@keysight.com>
2018-10-17 16:44:13 -0400

    pypi versioning test

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pyarbtools', version='0.1.0',
-    author='Morgan Allison', author_email='morgan.allison@keysight.com',
+    name='pyarbtools',
+    version='0.0.2',
+    author='Morgan Allison',
+    author_email='morgan.allison@keysight.com',
     description='Keysight signal generator tools.',
     long_description='A collection of Python classes and functions that provide signal creation and instrument control capabilities for Keysight signal sources.',
-    url='https://github.com/morgan-at-keysight/pyArbTools',
+    url='https://github.com/morgan-at-keysight/pyarbtools',
     packages=find_packages(exclude=['docs', 'tests']),
-    install_requires=['numpy', 'scipy'],
+    install_requires=['numpy > 1.14', 'scipy'],
     # package_data={'pyarbtools':['package_data.dat']}
     license='GPL 3',
-    classifiers=['License :: GNU GPL 3 License',
-        'Programming Language :: Python :: 3.6',]
+    classifiers=['License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.6',],
 )
