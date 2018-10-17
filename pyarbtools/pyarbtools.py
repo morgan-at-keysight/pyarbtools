6ee073ea08b76b60bcc0c1c0dac5fe50e196c03f
morgan-at-keysight <morgan.allison@keysight.com>
2018-10-17 14:34:07 -0400

    Removed unused import statements. Fixed case of AWGError and VSGError. Made static methods explicitly static. Removed unused argument from VSG.configure(). Separated waveform creation methods into standalone module.

--- pyarbtools/pyarbtools.py
+++ pyarbtools/pyarbtools.py
@@ -11,8 +11,8 @@ Tested on M8190A, M8195A, N5194A, N5182B, E8257D
 """
 
 from socket_instrument import *
-from scipy.signal import max_len_seq
 from scipy.io import loadmat
+import wfmBuilder
 
 
 class AWGError(Exception):
@@ -65,9 +65,9 @@ class M8190A(SocketInstrument):
 
         rl = len(wfm)
         if rl < self.minLen:
-            raise AwgError(f'Waveform length: {rl}, must be at least {self.minLen}.')
+            raise AWGError(f'Waveform length: {rl}, must be at least {self.minLen}.')
         if rl % self.gran != 0:
-            raise AwgError(f'Waveform must have a granularity of {self.gran}.')
+            raise AWGError(f'Waveform must have a granularity of {self.gran}.')
 
         return np.array(self.binMult * wfm, dtype=np.int16) << self.binShift
 
@@ -118,7 +118,8 @@ class M8190A(SocketInstrument):
         self.res = self.query('trace1:dwidth?').strip().lower()
         self.check_resolution()
 
-    def iq_wfm_combiner(self, i, q):
+    @staticmethod
+    def iq_wfm_combiner(i, q):
         """Combines i and q wfms into a single interleaved wfm for download to AWG."""
         iq = np.empty(2 * len(i), dtype=np.int16)
         iq[0::2] = i
@@ -153,7 +154,7 @@ class M8190A(SocketInstrument):
             elif self.intFactor == 24 or self.intFactor == 48:
                 self.idleGran = 1
         else:
-            raise AwgError('Invalid resolution selected.')
+            raise AWGError('Invalid resolution selected.')
 
     def download_wfm(self, wfm, ch=1):
         """Defines and downloads a waveform into the segment memory."""
@@ -212,9 +213,9 @@ class M8195A(SocketInstrument):
 
         rl = len(wfm)
         if rl < self.minLen:
-            raise AwgError(f'Waveform length: {rl}, must be at least {self.minLen}.')
+            raise AWGError(f'Waveform length: {rl}, must be at least {self.minLen}.')
         if rl % self.gran != 0:
-            raise AwgError(f'Waveform must have a granularity of {self.gran}.')
+            raise AWGError(f'Waveform must have a granularity of {self.gran}.')
 
         return np.array(self.binMult * wfm, dtype=np.int8) << self.binShift
 
@@ -271,12 +272,12 @@ class VSG(SocketInstrument):
         elif 'bbg' in self.refSrc.lower():
             self.refFreq = float(self.query('roscillator:frequency:bbg?').strip())
         else:
-            raise VsgError('Unknown refSrc selected.')
+            raise VSGError('Unknown refSrc selected.')
         self.gran = 2
         self.minLen = 60
         self.binMult = 32767
 
-    def configure(self, rfState=0, modState=0, cf=1e9, amp=-130, iqScale=70, refSrc='int', refFreq=10e6, fs=200e6):
+    def configure(self, rfState=0, modState=0, cf=1e9, amp=-130, iqScale=70, refSrc='int', fs=200e6):
         """Sets basic configuration for VSG and populates class attributes accordingly."""
         self.write(f'output {rfState}')
         self.rfState = self.query('output?').strip()
@@ -295,7 +296,7 @@ class VSG(SocketInstrument):
         elif 'bbg' in self.refSrc.lower():
             self.refFreq = float(self.query('roscillator:frequency:bbg?').strip())
         else:
-            raise VsgError('Unknown refSrc selected.')
+            raise VSGError('Unknown refSrc selected.')
         self.write(f'radio:arb:sclock:rate {fs}')
         self.fs = float(self.query('radio:arb:sclock:rate?').strip())
         self.write(f'radio:arb:rscaling {iqScale}')
@@ -326,7 +327,8 @@ class VSG(SocketInstrument):
         self.binblockwrite(f'mmemory:data "wfm1:{name}", ', iq)
         self.write(f'radio:arb:waveform "WFM1:{name}"')
 
-    def iq_wfm_combiner(self, i, q):
+    @staticmethod
+    def iq_wfm_combiner(i, q):
         """Combines i and q wfms into a single wfm for download to internal arb."""
         iq = np.empty(2 * len(i), dtype=np.int16)
         iq[0::2] = i
@@ -343,10 +345,10 @@ class VSG(SocketInstrument):
 
         rl = len(wfm)
         if rl < self.minLen:
-            raise VsgError(f'Waveform length: {rl}, must be at least {self.minLen}.')
+            raise VSGError(f'Waveform length: {rl}, must be at least {self.minLen}.')
         if rl % self.gran != 0:
             # vsg.query('*opc?')
-            raise VsgError(f'Waveform must have a granularity of {self.gran}.')
+            raise VSGError(f'Waveform must have a granularity of {self.gran}.')
 
         if bigEndian:
             return np.array(self.binMult * wfm, dtype=np.int16).byteswap()
@@ -400,7 +402,7 @@ class UXG(SocketInstrument):
         elif 'bbg' in self.refSrc.lower():
             self.refFreq = float(self.query('roscillator:frequency:bbg?').strip())
         else:
-            raise VsgError('Unknown refSrc selected.')
+            raise VSGError('Unknown refSrc selected.')
         self.write(f'radio:arb:sclock:rate {fs}')
         self.fs = float(self.query('radio:arb:sclock:rate?').strip())
         self.write(f'radio:arb:rscaling {iqScale}')
@@ -417,7 +419,8 @@ class UXG(SocketInstrument):
         self.lanStream.shutdown(socket.SHUT_RDWR)
         self.lanStream.close()
 
-    def bin_pdw_builder(self, operation=0, freq=1e9, phase=0, startTimeSec=0, power=0, markers=0,
+    @staticmethod
+    def bin_pdw_builder(operation=0, freq=1e9, phase=0, startTimeSec=0, power=0, markers=0,
                         phaseControl=0, rfOff=0, wIndex=0, wfmMkrMask=0):
         """This function builds a single format-1 PDW from a list of parameters.
 
@@ -579,7 +582,8 @@ class UXG(SocketInstrument):
         if assign:
             self.write(f'radio:arb:waveform "WFM1:{name}"')
 
-    def iq_wfm_combiner(self, i, q):
+    @staticmethod
+    def iq_wfm_combiner(i, q):
         """Combines i and q wfms into a single wfm for download to AWG."""
         iq = np.empty(2 * len(i), dtype=np.uint16)
         iq[0::2] = i
@@ -596,315 +600,11 @@ class UXG(SocketInstrument):
 
         rl = len(wfm)
         if rl < self.minLen:
-            raise VsgError(f'Waveform length: {rl}, must be at least {self.minLen}.')
+            raise VSGError(f'Waveform length: {rl}, must be at least {self.minLen}.')
         if rl % self.gran != 0:
-            raise VsgError(f'Waveform must have a granularity of {self.gran}.')
+            raise VSGError(f'Waveform must have a granularity of {self.gran}.')
 
         if bigEndian:
             return np.array(self.binMult * wfm, dtype=np.uint16).byteswap()
         else:
             return np.array(self.binMult * wfm, dtype=np.uint16)
-
-
-# Waveform Creation Methods
-
-
-def chirp_generator(length=100e-6, fs=100e6, chirpBw=20e6, zeroLast=False):
-    """Generates a symmetrical linear chirp at baseband. Chirp direction
-    is determined by the sign of chirpBw (pos=up chirp, neg=down chirp)."""
-
-    """Define baseband iq waveform. Create a time vector that goes from
-    -1/2 to 1/2 instead of 0 to 1. This ensures that the chirp will be
-    symmetrical around the carrier."""
-
-    rl = fs * length
-    chirpRate = chirpBw / length
-    t = np.linspace(-rl / fs / 2, rl / fs / 2, rl, endpoint=False)
-
-    """Direct phase manipulation was used to create the chirp modulation.
-    https://en.wikipedia.org/wiki/Chirp#Linear
-    phase = 2*pi*(f0*t + k/2*t^2)
-    Since this is a baseband modulation scheme, there is no f0 term and the
-    factors of 2 cancel out. It looks odd to have a pi multiplier rather than
-    2*pi, but the math works out correctly. Just throw that into the complex
-    exponential function and you're off to the races."""
-
-    mod = np.pi * chirpRate * t**2
-    iq = np.exp(1j * mod)
-    if zeroLast:
-        iq[-1] = 0 + 1j*0
-    i = np.real(iq)
-    q = np.imag(iq)
-
-    return i, q
-
-
-def barker_generator(length=100e-6, fs=100e6, code='b2', zeroLast=False):
-    """Generates a baseband Barker phase coded signal."""
-
-    # Codes taken from https://en.wikipedia.org/wiki/Barker_code
-    barkerCodes = {'b2': [1, -1], 'b3': [1, 1, -1],
-                   'b41': [1, 1, -1, 1], 'b42': [1, 1, 1, -1],
-                   'b5': [1, 1, 1, -1, 1], 'b7': [1, 1, 1, -1, -1, 1, -1],
-                   'b11': [1, 1, 1, -1, -1, -1, 1, -1, -1, 1, -1],
-                   'b13': [1, 1, 1, 1, 1, -1, -1, 1, 1, -1, 1, -1, 1]}
-
-    # Create array for each phase shift and concatenate them
-    codeSamples = int(length / len(barkerCodes[code]) * fs)
-    barker = []
-    for p in barkerCodes[code]:
-        temp = np.full((codeSamples,), p)
-        barker = np.concatenate([barker, temp])
-
-    mod = np.pi / 2 * barker
-    iq = np.exp(1j * mod)
-
-    if zeroLast:
-        iq[-1] = 0 + 0j
-    i = np.real(iq)
-    q = np.imag(iq)
-
-    return i, q
-
-
-def rrc_filter(taps, a, symRate, fs):
-    """Generates the impulse response of a root raised cosine filter
-    from user-defined number of taps, rolloff factor, symbol rate,
-    and sample rate.
-    RRC equation taken from https://en.wikipedia.org/wiki/Root-raised-cosine_filter"""
-
-    dt = 1 / fs
-    tau = 1 / symRate
-    time = np.linspace(-taps / 2, taps / 2, taps, endpoint=False) * dt
-    h = np.zeros(taps, dtype=float)
-
-    for t, x in zip(time, range(len(h))):
-        if t == 0.0:
-            h[x] = 1.0 + a * (4 / np.pi - 1)
-        elif a != 0 and (t == tau/(4*a) or t == -tau/(4*a)):
-            h[x] = a / np.sqrt(2) * (((1 + 2 / np.pi) * (np.sin(np.pi / (4 * a))))
-            + ((1 - 2 / np.pi) * (np.cos(np.pi / (4 * a)))))
-        else:
-            h[x] = (np.sin(np.pi * t / tau * (1 - a)) + 4 * a * t / tau * np.cos(np.pi * t / tau * (1 + a)))\
-            / (np.pi * t / tau * (1 - (4 * a * t / tau) ** 2))
-
-    return time, h
-
-
-def rc_filter(taps, a, symRate, fs):
-    """Generates the impulse response of a raised cosine filter
-    from user-defined number of taps, rolloff factor, symbol rate,
-    and sample rate.
-    RC equation taken from https://en.wikipedia.org/wiki/Raised-cosine_filter"""
-
-    dt = 1 / fs
-    tau = 1 / symRate
-    time = np.linspace(-taps / 2, taps / 2, taps, endpoint=False) * dt
-    h = np.zeros(taps, dtype=float)
-
-    for t, x in zip(time, range(len(h))):
-        if t == 0.0:
-            h[x] = 1.0
-        elif a != 0 and (t == tau / (2 * a) or t == -tau / (2 * a)):
-            h[x] = np.pi / (4 * tau) * np.sinc(1 / (2 * a))
-        else:
-            h[x] = 1 / tau * np.sinc(t / tau) * np.cos(np.pi * a * t / tau) / (1 - (2 * a * t / tau) ** 2)
-
-    return time, h
-
-
-def bpsk_modulator(data, customMap=None):
-    """Converts list of bits to symbol values as strings, maps each
-    symbol value to a position on the complex plane, and returns an
-    array of complex values for BPSK.
-
-    customMap is a dict whos keys are strings containing the symbol's
-    binary value and whos values are the symbol's location in the
-    complex plane.
-    e.g. customMap = {'0101': 0.707 + 0.707j, ...} """
-
-    pattern = [str(d) for d in data]
-    if customMap:
-        bpskMap = customMap
-    else:
-        bpskMap = {'0': 1 + 0j, '1': -1 + 0j}
-
-    try:
-        return np.array([bpskMap[p] for p in pattern])
-    except KeyError:
-        raise ValueError('Invalid BPSK symbol value.')
-
-
-def qpsk_modulator(data, customMap=None):
-    """Converts list of bits to symbol values as strings, maps each
-    symbol value to a position on the complex plane, and returns an
-    array of complex values for QPSK.
-
-    customMap is a dict whos keys are strings containing the symbol's
-    binary value and whos values are the symbol's location in the
-    complex plane.
-    e.g. customMap = {'0101': 0.707 + 0.707j, ...}
-    """
-
-    pattern = [str(d0) + str(d1) for d0, d1 in zip(data[0::2], data[1::2])]
-    if customMap:
-        qpskMap = customMap
-    else:
-        qpskMap = {'00': 1 + 1j, '01': -1 + 1j, '10': -1 - 1j, '11': 1 - 1j}
-
-    try:
-        return np.array([qpskMap[p] for p in pattern])
-    except KeyError:
-        raise ValueError('Invalid QPSK symbol.')
-
-
-def psk8_modulator(data, customMap=None):
-    """Converts list of bits to symbol values as strings, maps each
-    symbol value to a position on the complex plane, and returns an
-    array of complex values for 8-PSK.
-
-    customMap is a dict whos keys are strings containing the symbol's
-    binary value and whos values are the symbol's location in the
-    complex plane.
-    e.g. customMap = {'0101': 0.707 + 0.707j, ...}
-    """
-
-    pattern = [str(d0) + str(d1) + str(d2) for d0, d1, d2 in
-               zip(data[0::3], data[1::3], data[2::3])]
-    if customMap:
-        psk8Map = customMap
-    else:
-        psk8Map = {'000': 1 + 0j, '001': 0.707 + 0.707j, '010': 0 + 1j,
-                   '011': -0.707 + 0.707j, '100': -1 + 0j,
-                   '101': -0.707 - 0.707j, '110': 0 - 1j,
-                   '111': 0.707 - 0.707j}
-
-    try:
-        return np.array([psk8Map[p] for p in pattern])
-    except KeyError:
-        raise ValueError('Invalid 8PSK symbol.')
-
-
-def qam16_modulator(data, customMap=None):
-    """Converts list of bits to symbol values as strings, maps each
-    symbol value to a position on the complex plane, and returns an
-    array of complex values for 16 QAM.
-
-    A 4-variable Karnaugh map is used to determine the default symbol
-    locations to prevent adjacent symbol errors from differing more
-    than 1 bit from the intended symbol.
-    https://www.gaussianwaves.com/2012/10/constructing-a-rectangular-constellation-for-16-qam/
-
-    customMap is a dict whos keys are strings containing the symbol's
-    binary value and whos values are the symbol's location in the
-    complex plane.
-    e.g. customMap = {'0101': 0.707 + 0.707j, ...} """
-
-    pattern = [str(d0) + str(d1) + str(d2) + str(d3) for d0, d1, d2, d3 in
-               zip(data[0::4], data[1::4], data[2::4], data[3::4])]
-    if customMap:
-        qamMap = customMap
-    else:
-        qamMap = {'0000': -3 - 3j, '0001': -3 - 1j, '0010': -3 + 3j,
-                  '0011': -3 + 1j, '0100': -1 - 3j, '0101': -1 - 1j,
-                  '0110': -1 + 3j, '0111': -1 + 1j, '1000': 3 - 3j,
-                  '1001': 3 - 1j, '1010': 3 + 3j, '1011': 3 + 1j,
-                  '1100': 1 - 3j, '1101': 1 - 1j, '1110': 1 + 3j,
-                  '1111': 1 + 1j}
-    try:
-        return np.array([qamMap[p] for p in pattern])
-    except KeyError:
-        raise ValueError('Invalid 16 QAM symbol.')
-
-
-def qam32_modulator(data, customMap=None):
-    """Converts list of bits to symbol values as strings, maps each
-    symbol value to a position on the complex plane, and returns an
-    array of complex values for 32 QAM.
-
-    A 5-variable Karnaugh map is used to determine the default symbol
-    locations to prevent adjacent symbol errors from differing more
-    than 1 bit from the intended symbol.
-
-    customMap is a dict whos keys are strings containing the symbol's
-    binary value and whos values are the symbol's location in the
-    complex plane.
-    e.g. customMap = {'0101': 0.707 + 0.707j, ...} """
-
-    pattern = [str(d0) + str(d1) + str(d2) + str(d3) + str(d4) for d0, d1, d2, d3, d4 in
-               zip(data[0::5], data[1::5], data[2::5], data[3::5], data[4::5])]
-    if customMap:
-        qamMap = customMap
-    else:
-        qamMap = {'0000': -3 - 3j, '0001': -3 - 1j, '0010': -3 + 3j,
-                  '0011': -3 + 1j, '0100': -1 - 3j, '0101': -1 - 1j,
-                  '0110': -1 + 3j, '0111': -1 + 1j, '1000': 3 - 3j,
-                  '1001': 3 - 1j, '1010': 3 + 3j, '1011': 3 + 1j,
-                  '1100': 1 - 3j, '1101': 1 - 1j, '1110': 1 + 3j,
-                  '1111': 1 + 1j}
-    try:
-        return np.array([qamMap[p] for p in pattern])
-    except KeyError:
-        raise ValueError('Invalid 16 QAM symbol.')
-
-
-def digmod_prbs_generator(modType, fs, symRate, prbsOrder=9, filt=rrc_filter, alpha=0.35):
-    """Generates a baseband modulated signal with a given modulation
-    type and root raised cosine filter using PRBS data."""
-
-    saPerSym = int(fs / symRate)
-    filterSymbolLength = 10
-
-    # Define bits per symbol and modulator function based on modType
-    if modType.lower() == 'bpsk':
-        bitsPerSym = 1
-        modulator = bpsk_modulator
-    elif modType.lower() == 'qpsk':
-        bitsPerSym = 2
-        modulator = qpsk_modulator
-    elif modType.lower() == '8psk':
-        bitsPerSym = 3
-        modulator = psk8_modulator
-    elif modType.lower() == 'qam16':
-        bitsPerSym = 4
-        modulator = qam16_modulator
-    else:
-        raise ValueError('Invalid modType chosen.')
-
-    # Create pattern and repeat to ensure integer number of symbols.
-    temp, state = max_len_seq(prbsOrder)
-    bits = temp
-    repeats = 1
-    while len(bits) % bitsPerSym:
-        bits = np.tile(temp, repeats)
-        repeats += 1
-
-    """Convert the pseudorandom bit sequence, which is a list of bits,
-    into the binary values of symbols as strings, and then map symbols
-    to locations in the complex plane."""
-    symbols = modulator(bits)
-
-    """Perform a pseudo circular convolution on the symbols to mitigate
-    zeroing of samples due to filter delay (i.e. PREpend the
-    last few symbols and APpend the first few symbols)."""
-    symbols = np.concatenate((symbols[-int(filterSymbolLength/2):], symbols, symbols[:int(filterSymbolLength/2)]))
-
-    """Zero-fill each symbol rather than repeating the symbol value to
-    fill. This is to ensure the filter operates on an impulse response
-    rather than a zero-order hold response."""
-    iq = np.zeros(len(symbols) * saPerSym, dtype=np.complex)
-    iq[::saPerSym] = symbols
-
-    """Create pulse shaping filter. Taps should be an odd number to 
-    ensure there is a tap in the center of the filter."""
-    taps = filterSymbolLength * saPerSym + 1
-    time, filter = filt(int(taps), alpha, symRate, fs)
-
-    # Apply filter and trim off zeroed samples to ensure EXACT wraparound.
-    iq = np.convolve(iq, filter)
-    iq = iq[taps-1:-taps+1]
-    # Scale waveform data
-    sFactor = abs(np.amax(iq))
-    iq = iq / sFactor * 0.707
-
-    return np.real(iq), np.imag(iq)
