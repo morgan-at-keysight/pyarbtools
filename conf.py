a2156ccc4c2b43d0460729b89192d3ab0821d224
morgan-at-keysight <morgan.allison@keysight.com>
2018-11-26 12:35:58 -0500

    Updated docs and PyPI peripheral code.

--- conf.py
+++ conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Morgan Allison'
 author = 'Morgan Allison'
 
 # The short X.Y version
-version = '0.0.7'
+version = '0.0.8'
 # The full version, including alpha/beta/rc tags
-release = '0.0.7'
+release = '0.0.8'
 
 
 # -- General configuration ---------------------------------------------------
