1d94b454868314489f144f657f84fe3871050919
morgan-at-keysight <morgan.allison@keysight.com>
2018-12-14 10:00:05 -0500

    Updated docs for M8196A and updated to version 0.0.11

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyarbtools',
-    version='0.0.10',
+    version='0.0.11',
     author='Morgan Allison',
     author_email='morgan.allison@keysight.com',
     description='Keysight signal generator tools.',
