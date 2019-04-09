6902d2398809597ec07d3849961fb588d8cb2cf0
morgan-at-keysight <morgan.allison@keysight.com>
2019-04-09 18:23:54 -0400

    usage.rst: updated documentation to reflect changes to codebase. Updated versioning.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyarbtools',
-    version='0.0.12',
+    version='0.0.13',
     author='Morgan Allison',
     author_email='morgan.allison@keysight.com',
     description='Keysight signal generator tools.',
