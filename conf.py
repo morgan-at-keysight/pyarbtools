f1e8bf49ca4b38c427180046467584b8ea47cc8d
morgan-at-keysight <morgan.allison@keysight.com>
2019-11-12 17:49:38 -0800

    instruments.py: removed "output on" call in Vector/AnalogUXG stream_play methods. Updated docs

--- conf.py
+++ conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Morgan Allison'
 author = 'Morgan Allison'
 
 # The short X.Y version
-version = '0.0.13'
+version = '0.0.14'
 # The full version, including alpha/beta/rc tags
-release = '0.0.13'
+release = '0.0.14'
 
 
 # -- General configuration ---------------------------------------------------
