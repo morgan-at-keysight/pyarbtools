3db2a1d7e53c96f3509757f04af80228d34b7329
morgan-at-keysight <morgan.allison@keysight.com>
2020-05-13 15:45:43 -0700

    conf.py: Mock imports for documentation

--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
+autodoc_mock_imports = ["numpy"]
+
 import pyarbtools
 
 # -- General configuration ---------------------------------------------
