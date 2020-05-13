355844c883e7985c0375808eb03532eae1fa4fd2
morgan-at-keysight <morgan.allison@keysight.com>
2020-05-13 15:33:58 -0700

    conf.py: Forgot to import pyarbtools into conf.py for version checking

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../pyarbtools'))
 
-# import pyarbtools
+import pyarbtools
 
 # -- General configuration ---------------------------------------------
 
